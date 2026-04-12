c038ed0582db4a4f95d964a10f8e7243b02637ac
jnvoigt <jnvoigt@outlook.de>
2026-04-12 18:05:25 +0200

    fix sync command and program to correctly use system command line api

--- Lingo.Cli/Program.cs
+++ Lingo.Cli/Program.cs
@@ -9,7 +9,7 @@ public static class Program
     public static async Task Main(params string[] args)
     {
         var rootCommand = new RootCommand("Lingo localization tool");
-        rootCommand.AddCommand(SyncCommand.GetCommand());
-        await rootCommand.InvokeAsync(args);
+        rootCommand.Add(SyncCommand.GetCommand());
+        await rootCommand.Parse(args).InvokeAsync();
     }
 }
